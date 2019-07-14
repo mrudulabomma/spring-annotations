d71335e567033814d45c8aecb2061b2d56215462
Mrudula <mrudulareddy859@gmail.com>
2019-07-14 22:28:40 +0530

    task6

---
+++ src/main/java/com/stackroute/Application.java
@@ -0,0 +1,24 @@
+package com.stackroute;
+
+import com.stackroute.demo.AppConfig;
+import com.stackroute.demo.BeanLifeCycleDemo;
+import com.stackroute.demo.BeanPostProcessorDemo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args ) {
+        ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+        ((AnnotationConfigApplicationContext) context).registerShutdownHook();
+        BeanLifeCycleDemo beanLifeCycleDemo=(BeanLifeCycleDemo) context.getBean("beanLifeCycle");
+        BeanPostProcessorDemo bd=(BeanPostProcessorDemo)context.getBean("beanPostProcessor");
+
+
+    }
+}
