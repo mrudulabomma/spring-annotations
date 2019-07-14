7165c92fa31539790ef092de113481bc66c9e999
Mrudula <mrudulareddy859@gmail.com>
2019-07-14 22:21:31 +0530

    task4

---
+++ src/main/java/com/stackroute/Application.java
@@ -0,0 +1,35 @@
+package com.stackroute;
+
+import com.stackroute.domain.AppConfig;
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args )
+    {
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        Movie movie=(Movie)applicationContext.getBean("firstMovie");
+        Movie movie1=(Movie)applicationContext.getBean("secondMovie");
+        movie.setApplicationContext(applicationContext);
+        movie.setBeanName("mudhhuuu");
+
+        System.out.println( movie.getActor());
+        System.out.println( movie1.getActor());
+
+        BeanFactory xmlBeanFactory= new AnnotationConfigApplicationContext(AppConfig.class);
+        Movie movieFirst= (Movie)xmlBeanFactory.getBean("secondMovie");
+        Movie moviesecond=(Movie)xmlBeanFactory.getBean("secondMovie");
+        movieFirst.setBeanFactory(xmlBeanFactory);
+    }
+}
